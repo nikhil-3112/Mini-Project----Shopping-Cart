4ecb9ece88f6ec269813ab1ef2888394f0a50c47
Nikhil Chaudhari <nikhil.l.c31@outlook.com>
2024-03-12 01:28:53 +0530

    commit changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Make sure this path is correct
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Cart</h1>
+    </div>
+  );
+}
+
+export default App;
