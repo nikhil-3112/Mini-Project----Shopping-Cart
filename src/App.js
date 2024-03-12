eaf84d20f20b637752531223eb08c7035206b163
Nikhil Chaudhari <nikhil.l.c31@outlook.com>
2024-03-12 12:46:26 +0530

    commit chages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'; // Make sure this path is correct
+// import ReactDOM from 'react-dom';
+import CartItem from './cartItem'; 
 
 function App() {
   return (
     <div className="App">
-      <h1>Cart</h1>
+      <CartItem />
     </div>
   );
 }
