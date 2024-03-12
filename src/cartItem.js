7a7d1a200ddb6f8d2fa6f06fdde2aa697d39008b
Nikhil Chaudhari <nikhil.l.c31@outlook.com>
2024-03-12 19:49:01 +0530

    Add CartItem Component

--- src/cartItem.js
+++ src/cartItem.js
@@ -1,18 +1,30 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+    constructor () {
+        super()
+        this.state = {
+            price: 999,
+            title: "Phone",
+            Qty: 1,
+            img: ''
+        }
+    }
     render() {
+        const { title, price, Qty} = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style = {styles.image}  />
                 </div>
                 <div className="right-block">
-                    <div style = { { fontSize:25}}>Phone</div>
-                    <div style = { { color:'#777'}}>Rs 1000</div>
-                    <div style = { { color:'#777'}}>Qty: 1</div>
+                    <div style = { { fontSize:25}}>Title: {title}</div>
+                    <div style = { { color:'#777'}}>Price: {price}</div>
+                    <div style = { { color:'#777'}}>Qty: {Qty}</div>
                     <div class="cart-item-actions">
-                        {/* { Buttons } */}
+                        <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/262/262038.png" />
+                        <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/659/659892.png" />
+                        <img alt="delete" className="action-icons" src="https://cdn.icon-icons.com/icons2/2249/PNG/512/delete_circle_outline_icon_139695.png" />
                     </div>
                 </div>
             </div>
